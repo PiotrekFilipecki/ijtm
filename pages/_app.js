5fd00d6cba7fae5e3c523d271ec25039e33bb4dc
Piotr Filipecki <piotrekfilipecki@gmail.com>
2023-04-04 17:28:53 +0200

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,25 @@
 import '../styles/globals.css'
+import { motion, AnimatePresence } from 'framer-motion'
+
+const variants = {
+  hidden: { opacity: 0},
+  enter: { opacity: 1},
+  exit: { opacity: 0},
+}
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <AnimatePresence
+    mode="wait"
+    //exitBeforeEnter
+
+    variants={variants}
+
+>
+
+    <Component {...pageProps} />
+    </AnimatePresence>
+  )
 }
 
 export default MyApp
